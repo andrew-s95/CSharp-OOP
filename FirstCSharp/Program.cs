82526cbdb551829d3407803a1f6611855859307f
andrew-s95 <56849472+andrew-s95@users.noreply.github.com>
2020-01-06 05:31:39 +0800

    First C# program
    Basic number loop

---
+++ FirstCSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FirstCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+        // loop from 1 to 5 excluding 5
+            for (int i = 1; i < 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+        }
+    }
+}
